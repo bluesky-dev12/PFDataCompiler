f41b6d510285169e23fcb19db86929ba529d3155
bluesky-dev12 <ravello17@gmail.com>
2024-03-05 02:01:18 +0000

    Added project
    Project added

---
+++ PathFinderTool/Core/Header.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PathFinderTool.Core
+{
+    public enum PathOperator
+    {
+        Invalid = 0,
+        Add = 1,
+        Sub = 2,
+        Mult = 3,
+        Div = 4,
+        Mod = 5,
+        Max = 6
+    }
+}
